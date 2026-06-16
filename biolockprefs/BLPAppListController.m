c32e292810d47e4446bab68857fd4456ce560c42
chqchshj <62175669+chqchshj@users.noreply.github.com>
2026-06-16 12:27:37 +0800

    v14.3: Rewrite BLPAppListController as UITableViewController with UISearchBar + UISwitch

--- biolockprefs/BLPAppListController.m
+++ biolockprefs/BLPAppListController.m
@@ -1,14 +1,22 @@
 #import "BLPAppListController.h"
-#import <Preferences/PSSpecifier.h>
 
 @implementation BLPAppListController
 
-- (NSArray *)specifiers {
-    if (!_specifiers) {
-        [self loadAppList];
-        _specifiers = [self buildSpecifiersFromArray:self.filteredApps];
-    }
-    return _specifiers;
+- (instancetype)init {
+    self = [super initWithStyle:UITableViewStyleInsetGrouped];
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"选择应用";
+    
+    [self loadAppList];
+    
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, self.tableView.bounds.size.width, 56)];
+    self.searchBar.placeholder = @"搜索应用名称";
+    self.searchBar.delegate = self;
+    self.tableView.tableHeaderView = self.searchBar;
 }
 
 - (void)loadAppList {
@@ -26,66 +34,90 @@
     self.filteredApps = self.allApps;
 }
 
-- (NSArray *)buildSpecifiersFromArray:(NSArray *)apps {
-    NSMutableArray *specs = [NSMutableArray array];
-    
-    PSSpecifier *group = [PSSpecifier preferenceSpecifierNamed:@"选择要保护的应用"
-                                                        target:self
-                                                           set:NULL
-                                                           get:NULL
-                                                        detail:Nil
-                                                          cell:PSGroupCell
-                                                          edit:Nil];
-    [specs addObject:group];
-    
-    for (NSDictionary *app in apps) {
-        NSString *bundleID = app[@"bundleID"];
-        NSString *name = app[@"name"];
-        
-        PSSpecifier *spec = [PSSpecifier preferenceSpecifierNamed:name
-                                                           target:self
-                                                              set:@selector(setProtected:forSpecifier:)
-                                                              get:@selector(getProtectedForSpecifier:)
-                                                           detail:Nil
-                                                             cell:PSSwitchCell
-                                                             edit:Nil];
-        [spec setProperty:bundleID forKey:@"bundleID"];
-        [spec setProperty:bundleID forKey:@"id"];
-        [specs addObject:spec];
+#pragma mark - Search
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if (searchText.length == 0) {
+        self.filteredApps = self.allApps;
+    } else {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name CONTAINS[cd] %@", searchText];
+        self.filteredApps = [self.allApps filteredArrayUsingPredicate:predicate];
+    }
+    [self.tableView reloadData];
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    [self.searchBar resignFirstResponder];
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.filteredApps.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"选择要保护的应用";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellID = @"AppCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
     }
     
-    return [specs copy];
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.title = @"选择应用";
-}
-
-- (id)getProtectedForSpecifier:(PSSpecifier *)specifier {
-    NSString *bundleID = [specifier propertyForKey:@"bundleID"];
+    NSDictionary *app = self.filteredApps[indexPath.row];
+    NSString *bundleID = app[@"bundleID"];
+    NSString *name = app[@"name"];
+    
+    cell.textLabel.text = name;
+    cell.detailTextLabel.text = bundleID;
+    cell.detailTextLabel.textColor = [UIColor grayColor];
+    
+    // Check if this app is protected
     NSString *prefsPath = @"/var/mobile/Library/Preferences/com.batues.biolock.plist";
     NSDictionary *prefs = [NSDictionary dictionaryWithContentsOfFile:prefsPath];
     NSArray *protectedApps = prefs[@"ProtectedApps"] ?: @[];
-    return @([protectedApps containsObject:bundleID]);
+    BOOL isProtected = [protectedApps containsObject:bundleID];
+    
+    // Add switch
+    UISwitch *toggle = [[UISwitch alloc] init];
+    toggle.on = isProtected;
+    toggle.tag = indexPath.row;
+    [toggle addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
+    cell.accessoryView = toggle;
+    
+    return cell;
 }
 
-- (void)setProtected:(id)value forSpecifier:(PSSpecifier *)specifier {
-    NSString *bundleID = [specifier propertyForKey:@"bundleID"];
+- (void)switchChanged:(UISwitch *)sender {
+    NSInteger row = sender.tag;
+    NSDictionary *app = self.filteredApps[row];
+    NSString *bundleID = app[@"bundleID"];
+    
     NSString *prefsPath = @"/var/mobile/Library/Preferences/com.batues.biolock.plist";
     NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:prefsPath] ?: [NSMutableDictionary dictionary];
     NSMutableArray *protectedApps = [prefs[@"ProtectedApps"] mutableCopy] ?: [NSMutableArray array];
     
-    BOOL enabled = [value boolValue];
-    if (enabled && ![protectedApps containsObject:bundleID]) {
+    if (sender.on && ![protectedApps containsObject:bundleID]) {
         [protectedApps addObject:bundleID];
-    } else if (!enabled) {
+    } else if (!sender.on) {
         [protectedApps removeObject:bundleID];
     }
     
     prefs[@"ProtectedApps"] = protectedApps;
     [prefs writeToFile:prefsPath atomically:YES];
     
+    // Notify Tweak to reload
     CFNotificationCenterPostNotification(
         CFNotificationCenterGetDarwinNotifyCenter(),
         CFSTR("com.batues.biolock/ReloadPrefs"),
